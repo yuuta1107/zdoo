d96cc0627b286ddbc37644fd430e3db4338aa772
liugang <liugang@cnezsoft.com>
2017-02-27 14:50:53 +0800

    * Adjust codes.

--- app/sys/upgrade/model.php
+++ app/sys/upgrade/model.php
@@ -129,6 +129,7 @@ class upgradeModel extends model
                 $this->moveDocContent();
                 $this->addProjectDoc();
             case '4_0':
+                $this->execSQL($this->getUpgradeFile('4.0'));
                 $this->addProjPrivilege();
             default: if(!$this->isError()) $this->loadModel('setting')->updateVersion($this->config->version);
         }
@@ -171,6 +172,7 @@ class upgradeModel extends model
             case '3_5'     : $confirmContent .= file_get_contents($this->getUpgradeFile('3.5'));
             case '3_6'     : $confirmContent .= file_get_contents($this->getUpgradeFile('3.6'));
             case '3_7'     : $confirmContent .= file_get_contents($this->getUpgradeFile('3.7'));
+            case '4_0'     : $confirmContent .= file_get_contents($this->getUpgradeFile('3.7'));
         }
         return $confirmContent;
     }
