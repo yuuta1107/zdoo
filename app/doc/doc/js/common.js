6be935f3ef7b38837488d5979b4afd3eaa2523bf
daitingting <daitingting@xirangit.com>
2017-05-19 16:26:28 +0800

    * Adjust style for sort.

--- app/doc/doc/js/common.js
+++ app/doc/doc/js/common.js
@@ -54,7 +54,7 @@ $(document).ready(function()
 
     $('#libList').sortable(
     {
-        trigger: '.icon-move',
+        trigger: '.lib',
         selector: '.lib',
         finish: function()
         {
