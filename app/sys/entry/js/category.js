814c9ddd2a3e35e4c04f3717c492baf5097de273
Catouse <catouse@gmail.com>
2017-06-13 10:00:10 +0800

    * fix refreshDesktop not work in iframe.

--- app/sys/entry/js/category.js
+++ app/sys/entry/js/category.js
@@ -2,9 +2,6 @@ $(document).ready(function()
 {
     $.setAjaxDeleter('.deleteCategory', function()
     {
-        $.getJSON(createLink('entry', 'getJSONEntries'), function(entries)
-        {
-            $.refreshDesktop(entries, true);
-        });
+        $.refreshDesktop();
     });
 })
