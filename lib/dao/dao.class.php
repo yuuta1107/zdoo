96e3bcc39caff4910577aa2bf3b14f985c4cc94b
wangyidong <yidong@cnezsoft.com>
2017-07-10 14:54:16 +0800

    * finish task #3663.

--- lib/dao/dao.class.php
+++ lib/dao/dao.class.php
@@ -20,6 +20,75 @@ helper::import(dirname(dirname(__FILE__)) . '/base/dao/dao.class.php');
  */
 class dao extends baseDAO
 {
+    /**
+     * 检查字段是否满足条件。
+     * Check a filed is satisfied with the check rule.
+     * 
+     * @param  string $fieldName    the field to check
+     * @param  string $funcName     the check rule
+     * @param  string $condition     the condition
+     * @access public
+     * @return object the dao object self.
+     */
+    public function check($fieldName, $funcName, $condition = '')
+    {
+        /* 
+         * 如果没数据中没有该字段，直接返回。
+         * If no this field in the data, return.
+         **/
+        if(!isset($this->sqlobj->data->$fieldName)) return $this;
+
+        /* 设置字段值。 */
+        /* Set the field label and value. */
+        global $lang, $config, $app;
+        $table = strtolower(substr($this->table, strrpos($this->table, '_') + 1));
+        $table = str_replace('`', '', $table);
+
+        $fieldLabel = isset($lang->$table->$fieldName) ? $lang->$table->$fieldName : $fieldName;
+        $value = isset($this->sqlobj->data->$fieldName) ? $this->sqlobj->data->$fieldName : null;
+
+        /* 
+         * 检查唯一性。
+         * Check unique.
+         **/
+        if($funcName == 'unique')
+        {
+            $args = func_get_args();
+            $sql  = "SELECT COUNT(*) AS count FROM $this->table WHERE `$fieldName` = " . $this->sqlobj->quote($value); 
+            if($condition) $sql .= ' AND ' . $condition;
+            try
+            {
+                $row = $this->dbh->query($sql)->fetch();
+                if($row->count != 0) $this->logError($funcName, $fieldName, $fieldLabel, array($value));
+            }
+            catch (PDOException $e) 
+            {
+                $this->sqlError($e);
+            }
+        }
+        else
+        {
+            /* 
+             * 创建参数。
+             * Create the params.
+             **/
+            $funcArgs = func_get_args();
+            unset($funcArgs[0]);
+            unset($funcArgs[1]);
+
+            for($i = 0; $i < VALIDATER::MAX_ARGS; $i ++)
+            {
+                ${"arg$i"} = isset($funcArgs[$i + 2]) ? $funcArgs[$i + 2] : null;
+            }
+            $checkFunc = 'check' . $funcName;
+            if(validater::$checkFunc($value, $arg0, $arg1, $arg2) === false)
+            {
+                $this->logError($funcName, $fieldName, $fieldLabel, $funcArgs);
+            }
+        }
+
+        return $this;
+    }
 }
 
 /**
