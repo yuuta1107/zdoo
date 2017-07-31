7435db0990a861cbaa8a1e4685d176a5175df7f1
liugang <liugang@cnezsoft.com>
2017-07-31 10:32:00 +0800

    * Adjust codes.

--- app/sys/user/control.php
+++ app/sys/user/control.php
@@ -143,7 +143,7 @@ class user extends control
         /* Save sign out info. */
         if(commonModel::isAvailable('attend') and isset($this->config->attend->mustSignOut) and $this->config->attend->mustSignOut == 'no') $this->loadModel('attend', 'oa')->signOut();
 
-        if(isset($this->app->user->id)) $this->loadModel('action')->create('user', $this->app->user->id, 'logout');
+        if(!empty($this->app->user->id)) $this->loadModel('action')->create('user', $this->app->user->id, 'logout');
 
         session_destroy();
         setcookie('keepLogin', 'false', $this->config->cookieLife, $this->config->webRoot);
