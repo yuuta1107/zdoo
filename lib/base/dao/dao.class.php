922e2278a66fc77a883b74fc5b8c0de08937ca26
guanxiying <guanxiying@xirangit.com>
2018-03-16 10:19:18 +0800

    * Fix groupby function.

--- lib/base/dao/dao.class.php
+++ lib/base/dao/dao.class.php
@@ -1987,7 +1987,7 @@ class baseSQL
     public function groupBy($groupBy)
     {
         if($this->inCondition and !$this->conditionIsTrue) return $this;
-        if(!preg_match('/^(\w+\.)?(`\w+`|\w+)( +(desc|asc))?( *(, *(\w+\.)?(`\w+`|\w+)( +(desc|asc))?)?)*$/i', $groupBy)) die("Order is bad request, The order is $orders");
+        $groupBy = $this->quote($groupBy);
         $this->sql .= ' ' . DAO::GROUPBY . " $groupBy";
         return $this;
     }
