105cbfafccb66825dfa40c4f2dcbf03045723c4a
liugang <liugang@cnezsoft.com>
2018-11-08 17:47:18 +0800

    * Add products of order to contract.

--- app/crm/contract/model.php
+++ app/crm/contract/model.php
@@ -277,10 +277,16 @@ class contractModel extends model
 
         if($contract->order)
         {
-            $product       = '';
-            $orderProducts = $this->dao->select('product')->from(TABLE_ORDER)->where('id')->in($contract->order)->fetchPairs();
-            foreach($orderProducts as $orderProduct) $product .= trim($product, ',') . ',';
-            if($product) $contract->product = ',' . $product;
+            $products = array();
+            $orders   = $this->dao->select('id, product')->from(TABLE_ORDER)->where('id')->in($contract->order)->fetchAll();
+            foreach($orders as $order)
+            {
+                foreach(explode(',', trim($order->product, ',')) as $product)
+                {
+                    $products[$product] = $product;
+                }
+            }
+            $contract->product = empty($products) ? '' : ',' . implode(',', $products) . ',';
         }
 
         $contract = $this->loadModel('file', 'sys')->processImgURL($contract, $this->config->contract->editor->create['id']);
@@ -367,10 +373,16 @@ class contractModel extends model
 
         if($oldContract->order != $contract->order)
         {
-            $product       = '';
-            $orderProducts = $this->dao->select('product')->from(TABLE_ORDER)->where('id')->in($contract->order)->fetchPairs();
-            foreach($orderProducts as $orderProduct) $product .= trim($product, ',') . ',';
-            if($product) $contract->product = ',' . $product;
+            $products = array();
+            $orders   = $this->dao->select('id, product')->from(TABLE_ORDER)->where('id')->in($contract->order)->fetchAll();
+            foreach($orders as $order)
+            {
+                foreach(explode(',', trim($order->product, ',')) as $product)
+                {
+                    $products[$product] = $product;
+                }
+            }
+            $contract->product = empty($products) ? '' : ',' . implode(',', $products) . ',';
         }
 
         $contract = $this->loadModel('file', 'sys')->processImgURL($contract, $this->config->contract->editor->edit['id']);
@@ -990,13 +1002,20 @@ class contractModel extends model
      */
     public function updateContractProduct($contractID)
     {
-        $product = '';
-        $orders  = $this->dao->select('`order`')->from(TABLE_CONTRACTORDER)->where('contract')->eq($contractID)->fetchPairs();
+        $products = array();
+        $orders   = $this->dao->select('`order`')->from(TABLE_CONTRACTORDER)->where('contract')->eq($contractID)->fetchPairs();
         if($orders)
         {
-            $orderProducts = $this->dao->select('product')->from(TABLE_ORDER)->where('id')->in($orders)->fetchPairs();
-            foreach($orderProducts as $orderProduct) $product .= trim($orderProduct, ',') . ',';
+            $orders = $this->dao->select('id, product')->from(TABLE_ORDER)->where('id')->in($orders)->fetchAll();
+            foreach($orders as $order)
+            {
+                foreach(explode(',', trim($order->product, ',')) as $product)
+                {
+                    $products[$product] = $product;
+                }
+            }
         }
+        $product = empty($products) ? '' : ',' . implode(',', $products) . ',';
         $this->dao->update(TABLE_CONTRACT)->set('product')->eq($product)->where('id')->eq($contractID)->exec();
 
         return !dao::isError();
