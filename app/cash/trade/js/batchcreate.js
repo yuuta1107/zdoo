4f857979b00932179ed8e125560922f242a9b7bc
liugang <liugang@cnezsoft.com>
2019-01-03 16:54:56 +0800

    * Fix bug when batch create trade the category is empty.

--- app/cash/trade/js/batchcreate.js
+++ app/cash/trade/js/batchcreate.js
@@ -5,8 +5,8 @@ $(document).ready(function()
     $(document).on('change', '.type', function()
     {
         var type = $(this).val();
-        $(this).parents('tr').find('.in, .out').next('.chosen-container').hide().attr('disabled', true).find('*').attr('disabled', true);
-        $(this).parents('tr').find('.' + type).next('.chosen-container').show().attr('disabled', false).find('*').attr('disabled', false);
+        $(this).parents('tr').find('.in, .out').attr('disabled', true).next('.chosen-container').hide().find('*').attr('disabled', true);
+        $(this).parents('tr').find('.' + type).attr('disabled', false).next('.chosen-container').show().find('*').attr('disabled', false);
         $(this).parents('tr').find('.in, .out').hide();
         $(this).parents('tr').find('div.' + type).show();
     })
