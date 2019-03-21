2a80c52bfbc23fe3c2a7c49adc150de457d5eca9
daitingting <daitingting@xirangit.com>
2019-03-21 13:55:32 +0800

    * Change hours field.

--- db/upgrade5.1.sql
+++ db/upgrade5.1.sql
@@ -1,2 +1,7 @@
 ALTER TABLE `sys_product` ADD `subject` mediumint(8) unsigned NOT NULL DEFAULT '0' AFTER `category`;
 ALTER TABLE `sys_category` ADD `deleted` enum('0', '1') NOT NULL DEFAULT '0';
+
+ALTER TABLE `oa_overtime` CHANGE `hours` `hours` float unsigned NOT NULL;
+ALTER TABLE `oa_leave` CHANGE `hours` `hours` float unsigned NOT NULL;
+ALTER TABLE `oa_lieu` CHANGE `hours` `hours` float unsigned NOT NULL;
+ALTER TABLE `sys_team` CHANGE `hours` `hours` float unsigned NOT NULL;
