b2a78719b02b31a58c442c5203b3012b2bf0d807
Catouse <catouse@gmail.com>
2019-05-22 17:40:35 +0800

    * code for task #4034, update custom zui for new UI.

--- app/sys/common/view/header.html.php
+++ app/sys/common/view/header.html.php
@@ -5,7 +5,7 @@
  * @copyright   Copyright 2009-2018 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
  * @license     ZPL (http://zpl.pub/page/zplv12.html)
  * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
- * @package     common 
+ * @package     common
  * @version     $Id: header.html.php 4029 2016-08-26 06:50:41Z liugang $
  * @link        http://www.ranzhi.org
  */
@@ -13,22 +13,29 @@
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
 <?php include 'header.lite.html.php';?>
 <style>body {padding-top: 58px}</style>
-<nav class='navbar navbar-main navbar-fixed-top' role='navigation' id='mainNavbar'>
-  <div class='navbar-header'>
-    <?php
-    if(isset($lang->app))
-    {
-        echo html::a($this->createLink($this->config->default->module), $lang->app->name, "class='navbar-brand'");
-    }
-    else
-    {
-        echo html::a($this->createLink('admin'), $lang->admin->common, "class='navbar-brand'");
-    }
-    ?>
+<div id='mainNavbar'>
+  <div class='container'>
+    <div class='heading'>
+      <?php
+      if(isset($lang->app))
+      {
+          echo html::a($this->createLink($this->config->default->module), $lang->app->name);
+      }
+      else
+      {
+          echo html::a($this->createLink('admin'), $lang->admin->common);
+      }
+      ?>
+    </div>
+    <nav>
+      <?php echo commonModel::createMainMenu($this->moduleName);?>
+    </nav>
+    <div class='actions'>
+      <button type='button' class='btn'>Blocks <span class='caret'></span></button>
+    </div>
   </div>
-  <?php echo commonModel::createMainMenu($this->moduleName);?>
-</nav>
-<?php 
+</div>
+<?php
 if(!isset($moduleMenu)) $moduleMenu = commonModel::createModuleMenu($this->moduleName);
 if($moduleMenu) echo "$moduleMenu\n<div class='row page-content with-menu'>\n"; else echo "<div class='row page-content'>";
 ?>
